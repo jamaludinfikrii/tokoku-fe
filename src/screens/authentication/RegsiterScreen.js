fbc75e6829ed4dd311d9c56e6712b92389e4689d
fikri <me@fikri.tech>
2020-06-04 12:06:55 +0700

    register

--- src/screens/authentication/RegsiterScreen.js
+++ src/screens/authentication/RegsiterScreen.js
@@ -1,8 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Container, Header, Content, Form, Item, Input, Label, Button,Text } from 'native-base';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, Alert } from 'react-native';
+import Axios from 'axios';
+import { API_URL } from '../../supports/constants/urlApi';
+import { saveUserData } from '../../redux/actions/userAction';
+import { connect } from 'react-redux';
+import OneSignal from 'react-native-onesignal';
 
 function RegisterScreen (props){
+  const [username,setUsername] = useState(null)
+  const [email,setEmail] = useState(null)
+  const [password,setPassword] = useState(null)
+  const [loading,setLoading] = useState(false)
+
+  const onBtnRegisterPress = () => {
+    // get value from input
+    
+    // check empty input
+    if(username && password && email){
+      Alert.alert('masyk')
+
+      OneSignal.push(function() {
+        OneSignal.on('subscriptionChange', function(isSubscribed) {
+          if (isSubscribed) {
+            // The user is subscribed
+            //   Either the user subscribed for the first time
+            //   Or the user was subscribed -> unsubscribed -> subscribed
+            OneSignal.getUserId( function(userId) {
+              // Make a POST call to your server with the user ID
+              Axios.post(API_URL + 'auth/register',{username,email,password,one_signal_id :userId})
+              .then((res) => {
+                // save data user ke global state
+                props.saveUserData(res.data[0])
+              })
+              .catch((err) => {
+                console.log(err)
+              })
+            });
+          }
+        });
+      });
+      
+      // send to server
+    }else{
+      Alert.alert('Error', 'Form cannot empty')
+    }
+    // send to backend / server
+  }
+
     return(
         <Container>
         <Header />
@@ -10,21 +55,17 @@ function RegisterScreen (props){
           <Form>
             <Item floatingLabel>
               <Label>Username</Label>
-              <Input />
+              <Input onChangeText={(text) => setUsername(text)} value={username} />
             </Item>
             <Item floatingLabel last>
               <Label>Email</Label>
-              <Input />
+              <Input onChangeText={(text) => setEmail(text)} value={email} />
             </Item>
             <Item floatingLabel last>
               <Label>Password</Label>
-              <Input />
+              <Input onChangeText={(text) => setPassword(text)} value={password}  />
             </Item>
-            <Item floatingLabel last>
-              <Label>Confirm Password</Label>
-              <Input />
-            </Item>
-            <Button style={{marginTop:20}} full primary>
+            <Button onPress={onBtnRegisterPress} style={{marginTop:20}} full primary>
                 <Text>Register</Text>
             </Button>
             <TouchableOpacity style={{marginTop:20}} onPress={() => props.navigation.navigate('login')}>
@@ -36,4 +77,4 @@ function RegisterScreen (props){
     )
 }
 
-export default RegisterScreen
+export default connect(null,{saveUserData})(RegisterScreen);
